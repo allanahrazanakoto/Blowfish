02215778a67b6578557cd57f17a06e4a51eecea9
allanahrazanakoto <allanahrazanakoto57@gmail.com>
2026-03-22 16:06:03 +0400

    Update ApiFileCipher.java

--- src/myBlowfish/ApiFileCipher.java
+++ src/myBlowfish/ApiFileCipher.java
@@ -1,9 +1,9 @@
 /**********************************************************************************
  * API: APIFileCipher - Suppose que la clé blowfish a déjà été généré
- * Description:	Utilise ApiBlowfish pour le chiffrement et déchiffrement de fichier
- * Auteur:		Didier Samfat
- * Date:		28 Mar 2021
- * Version:		1.0
+ * Description: Utilise ApiBlowfish pour le chiffrement et déchiffrement de fichier
+ * Auteur: Didier Samfat
+ * Date: 28 Mar 2021
+ * Version: 1.0
  *********************************************************************************/
 
 package myBlowfish;
@@ -16,100 +16,73 @@ import java.io.IOException;
 import java.security.Key;
 import java.util.Base64;
 
-
 public class ApiFileCipher {
 
-/**
- * Méthode qui lit un fichier est retourne ce qu'il a lu	
- * @param nomFichier: le fichier à lire
- * @return : la chaîne lue
- */
-	
-static String read(String nomFichier) {
-	
-	try {
-		  // Lit le fichier clair		
-		  File inFile = new File(nomFichier);
-		  FileInputStream inputStream = new FileInputStream(inFile);
-		  
-		  byte[] inBytes = new byte[(int) inFile.length()];
-		  inputStream.read(inBytes); // lit le fichier clair
-		  inputStream.close();
-		  
-		  //return Base64.getEncoder().encodeToString(inBytes); 
-		  
-		  return new String(inBytes);
-		  
-	    }
-	    catch (FileNotFoundException e) {}
-	    catch (IOException e) {}
-		return null;
-}
+    /**
+     * Méthode qui lit un fichier est retourne ce qu'il a lu
+     * @param nomFichier: le fichier à lire
+     * @return : la chaîne lue
+     */
+    static String read(String nomFichier) {
+        try {
+            File inFile = new File(nomFichier);
+            try (FileInputStream inputStream = new FileInputStream(inFile)) {
+                byte[] inBytes = new byte[(int) inFile.length()];
+                inputStream.read(inBytes);
+                return new String(inBytes);
+            }
+        } catch (FileNotFoundException e) {
+            // fichier non trouvé
+        } catch (IOException e) {
+            // erreur lecture
+        }
+        return null;
+    }
 
-/**
- * Méthode qui chiffre un fichier donné avec une clé blowfish
- * Créer asussi un nouveau fichier au format nomFichier.cryp 
- * @param nomFichier : fichier qui doit être chiffré
- * @param clef : doit être généré au prálable
- * @return : le texte chifré encode en Base64
- * @throws Exception
- */
-	
-static String encrypt(String nomFichier, Key clef) throws Exception{
-		
-		try {
-			  // Lit le fichier clair		
-			  File inFile = new File(nomFichier);
-			  FileInputStream inputStream = new FileInputStream(inFile);
-			  byte[] inBytes = new byte[(int) inFile.length()];
-			  inputStream.read(inBytes); // lit le fichier clair
-			  
-			  // creer fichier.txt.cryp
-			  String fichierCrypte = nomFichier + ".cryp";  
-			  File outFile = new File(fichierCrypte); // creer le fichier de sortie crypté
-			  FileOutputStream outputStream = new FileOutputStream(outFile);
-			 
-			  byte[] texteChiffre = ApiBlowfish.encryptInByte(inBytes, clef); 
-			  outputStream.write(texteChiffre); // on sauvegarde
+    /**
+     * Méthode qui chiffre un fichier donné avec une clé blowfish
+     * @param nomFichier : fichier qui doit être chiffré
+     * @param clef : doit être généré au préalable
+     * @return : le texte chiffré encodé en Base64
+     * @throws Exception
+     */
+    static String encrypt(String nomFichier, Key clef) throws Exception {
+        try {
+            File inFile = new File(nomFichier);
+            try (FileInputStream inputStream = new FileInputStream(inFile)) {
+                byte[] inBytes = new byte[(int) inFile.length()];
+                inputStream.read(inBytes);
 
-			  inputStream.close();
-			  outputStream.close();
-				
-			  // encodage pour lisibilité du texte à l'écran
-			  return Base64.getEncoder().encodeToString(texteChiffre); 
-			  
-		    }
-		    catch (FileNotFoundException e) {}
-		    catch (IOException e) {}
-			return null;
-		
-	}
-	
-	
-static String decrypt(String nomFichier, Key clef) throws Exception{
-	
-	try {
-		 		
-		  File inFile = new File(nomFichier);
-		  		  
-		  FileInputStream inputStream = new FileInputStream(inFile);
-			
-		  byte[] inBytes = new byte[(int) inFile.length()];
-		  inputStream.read(inBytes);
+                String fichierCrypte = nomFichier + ".cryp";
+                File outFile = new File(fichierCrypte);
+                try (FileOutputStream outputStream = new FileOutputStream(outFile)) {
+                    byte[] texteChiffre = ApiBlowfish.encryptInByte(inBytes, clef);
+                    outputStream.write(texteChiffre);
+                    return Base64.getEncoder().encodeToString(texteChiffre);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            // fichier non trouvé
+        } catch (IOException e) {
+            // erreur lecture/écriture
+        }
+        return null;
+    }
 
-		  byte[] texteDechiffre = ApiBlowfish.decryptInByte(inBytes, clef);
-		  
-		  String chaine = new String(texteDechiffre);
-		  
-		  inputStream.close();
-		  
-		  return chaine;
-		  
-	    }
-	    catch (FileNotFoundException e) {}
-	    catch (IOException e) {}
-		return null;
-}
-
-
+    static String decrypt(String nomFichier, Key clef) throws Exception {
+        try {
+            File inFile = new File(nomFichier);
+            try (FileInputStream inputStream = new FileInputStream(inFile)) {
+                byte[] inBytes = new byte[(int) inFile.length()];
+                inputStream.read(inBytes);
+                byte[] texteDechiffre = ApiBlowfish.decryptInByte(inBytes, clef);
+                return new String(texteDechiffre);
+            }
+        } catch (FileNotFoundException e) {
+            // fichier non trouvé
+        } catch (IOException e) {
+            // erreur lecture
+        }
+        return null;
+    }
 }
